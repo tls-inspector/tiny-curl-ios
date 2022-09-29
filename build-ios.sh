88554c12359def95f7be51b4cae5003b70725f4c
Ian Spence <ian@ecnepsnai.com>
2022-09-28 23:24:05 -0700

    Update for Xcode 14
    - Remove arm7,arm7s
    - Set minimum version to iOS 12
    - Remove bitcode

--- build-ios.sh
+++ build-ios.sh
@@ -23,7 +23,7 @@ fi
 
 export OUTDIR=output
 export BUILDDIR=build
-export IPHONEOS_DEPLOYMENT_TARGET="9.3"
+export IPHONEOS_DEPLOYMENT_TARGET="12.0"
 
 function build() {
     ARCH=$1
@@ -43,13 +43,16 @@ function build() {
 
     unset CFLAGS
     unset LDFLAGS
-    CFLAGS="-arch ${ARCH} -pipe -Os -gdwarf-2 -isysroot ${SDKDIR} -I${SDKDIR}/usr/include -miphoneos-version-min=${IPHONEOS_DEPLOYMENT_TARGET} -fembed-bitcode"
+    CFLAGS="-arch ${ARCH} -pipe -Os -gdwarf-2 -isysroot ${SDKDIR} -I${SDKDIR}/usr/include -miphoneos-version-min=${IPHONEOS_DEPLOYMENT_TARGET}"
     LDFLAGS="-arch ${ARCH} -isysroot ${SDKDIR}"
     export CFLAGS
     export LDFLAGS
     ./configure --host="${HOST}-apple-darwin" \
        --disable-shared \
        --enable-static \
+       --without-libidn2 \
+       --without-nghttp2 \
+       --without-nghttp3 \
        --with-secure-transport > "${LOG}" 2>&1
     make -j`sysctl -n hw.logicalcpu_max` >> "${LOG}" 2>&1
     cp lib/.libs/libcurl.a ../../$OUTDIR/libcurl-${ARCH}.a
@@ -61,8 +64,6 @@ mkdir $OUTDIR
 mkdir $BUILDDIR
 cd $BUILDDIR
 
-build armv7    armv7   `xcrun --sdk iphoneos --show-sdk-path`
-build armv7s   armv7s  `xcrun --sdk iphoneos --show-sdk-path`
 build arm64    arm     `xcrun --sdk iphoneos --show-sdk-path`
 build x86_64   x86_64  `xcrun --sdk iphonesimulator --show-sdk-path`
 
@@ -70,9 +71,8 @@ cd ../
 
 rm ${ARCHIVE}
 
-lipo -arch armv7 $OUTDIR/libcurl-armv7.a \
-   -arch armv7s $OUTDIR/libcurl-armv7s.a \
-   -arch arm64 $OUTDIR/libcurl-arm64.a \
+lipo \
+   -arch arm64  $OUTDIR/libcurl-arm64.a \
    -arch x86_64 $OUTDIR/libcurl-x86_64.a \
    -create -output $OUTDIR/libcurl_all.a
 
@@ -98,12 +98,3 @@ rm -rf $BUILDDIR
 rm -rf $OUTDIR
 
 cp "Info.plist" $FWNAME.framework/Info.plist
-
-set +e
-check_bitcode=$(otool -arch arm64 -l $FWNAME.framework/$FWNAME | grep __bitcode)
-if [ -z "$check_bitcode" ]
-then
-    echo "INFO: $FWNAME.framework doesn't contain Bitcode"
-else
-    echo "INFO: $FWNAME.framework contains Bitcode"
-fi
